b43abc8e7dfaba1e6903b27789090ee4d25be96a
Igor Barulin <igorbarulin@gmail.com>
2021-06-20 22:57:52 +0200

    update

--- Configuration.h
+++ Configuration.h
@@ -1828,17 +1828,17 @@
 //
 // Preheat Constants - Up to 5 are supported without changes
 //
-//#define PREHEAT_1_LABEL       "PLA"
-//#define PREHEAT_1_TEMP_HOTEND 185
-//#define PREHEAT_1_TEMP_BED     45
-//#define PREHEAT_1_TEMP_CHAMBER 35
-//#define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_1_LABEL       "PLA"
+#define PREHEAT_1_TEMP_HOTEND 170
+#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_CHAMBER 35
+#define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
-//#define PREHEAT_2_LABEL       "ABS"
-//#define PREHEAT_2_TEMP_HOTEND 240
-//#define PREHEAT_2_TEMP_BED     70
-//#define PREHEAT_2_TEMP_CHAMBER 35
-//#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "ABS"
+#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_TEMP_BED     60
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
 
 /**
  * Nozzle Park
